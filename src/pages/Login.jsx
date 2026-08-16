018e4eba2ae719224fcb1b05f9dedf0e5fe8fb85
chowi-14 <zoeclaudettereynaldo@gmail.com>
2026-08-16 23:47:53 +0800

    Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,51 @@
+import { useState } from "react"
 
 export default function Login() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [getMessage, setMessage] = useState("")
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    if (!username || !password ) {
+      setMessage("Please enter your username and password")
+      return
+    }
+
+    if (username === "admin" && password === "1234") {
+      setIsLoggedIn(true)
+      setMessage("Login Successful!")
+    } else {
+      setMessage("Invalid username or password")
+    }
+  }
+
+  const handleLogout = () => {
+    setIsLoggedIn(false),
+    setUsername("")
+    setPassword("")
+    setMessage("")
+  }
   return (
-    <div>Login</div>
+    <div>
+      <h2>Login Authentication</h2>
+
+      {!isLoggedIn ? (
+        <form onSubmit={handleLogin}>
+          <input type="text" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
+          <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+          <button type="submit">Login</button>
+          {getMessage && <p>{getMessage}</p>}
+        </form>
+      ) : (
+        <div>
+          <p>Welcome, {username}!</p>
+          <p>{getMessage}</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      )}
+
+    </div>
   )
 }
